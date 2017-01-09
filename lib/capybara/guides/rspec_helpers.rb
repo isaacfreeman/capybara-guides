733bc6ebb41828029a24f22147970a4d1aade6b8
Isaac Freeman <isaac@resolvedigital.co.nz>
2017-01-09 23:23:20 +1300

    Adds automatic recording of Capybara actions

--- lib/capybara/guides/rspec_helpers.rb
+++ lib/capybara/guides/rspec_helpers.rb
@@ -1,21 +1,91 @@
 module Capybara
   # Classes to support generating guides in Capybara feature specs
   module Guides
-    # TODO: Assume :guide tag if spec is in spec/guides
     def step(title = nil)
       @current_step = Step.new(self, @guide.steps.size + 1, title)
       @current_step.save_guide_screenshot
-      yield
       @guide.steps << @current_step
+      yield
     end
 
     def user_action(text)
       @current_step.actions << text
     end
-
-    # TODO: Represent user actions like button clicks
   end
 end
+
+# Override Capybara actions to record actions taken
+module RecordGuideActionForCapybaraActions
+  # TODO: attach_file(locator, path, options = {})
+
+  def check(locator, options = {})
+    record_action "Check \"#{locator}\""
+    super
+  end
+
+  def choose(locator, options = {})
+    record_action "Choose \"#{locator}\""
+    super
+  end
+
+  def click_button(locator = nil, options = {})
+    record_action "Click the \"#{locator}\" button"
+    super
+  end
+
+  def click_link(locator = nil, options = {})
+    record_action "Click on \"#{locator}\""
+    super
+  end
+
+  def click_link_or_button(locator = nil, options = {})
+    record_action "Click on \"#{locator}\""
+    super
+  end
+
+  def fill_in(locator, options = {})
+    record_action "Fill in #{locator} with \"#{options[:with]}\""
+    super
+  end
+
+  def select(value, options = {})
+    record_action "Select \"#{value}\" from #{options[:from]}"
+    super
+  end
+
+  def uncheck(locator, options = {})
+    record_action "Uncheck \"#{locator}\""
+    super
+  end
+
+  def unselect(value, options = {})
+    record_action "Unselect \"#{value}\" from #{options[:from]}"
+    super
+  end
+
+  private
+
+  # TODO: Merge with user_action
+  def record_action(action)
+    if RSpec.current_example.metadata[:current_guide].present?
+      current_step = RSpec.current_example.metadata[:current_guide].steps.last
+      current_step.actions << action if current_step.present?
+    end
+  end
+end
+Capybara::Node::Base.prepend RecordGuideActionForCapybaraActions
+
+# Override Capybara visit action to record the action
+module RecordGuideActionForVisit
+  def visit(visit_uri)
+    if RSpec.current_example.metadata[:current_guide].present?
+      current_step = RSpec.current_example.metadata[:current_guide].steps.last
+      current_step.actions << "Visit #{visit_uri}" if current_step.present?
+    end
+    super
+  end
+end
+Capybara::Session.prepend RecordGuideActionForVisit
 
 RSpec.configure do |config|
   config.include Capybara::Guides
@@ -27,6 +97,7 @@ RSpec.configure do |config|
 
   config.around(:each, :guide) do |example|
     @guide = Capybara::Guides::Guide.new(example.metadata[:description])
+    RSpec.current_example.metadata[:current_guide] = @guide
     example.run
     @guide.write_to_html
   end
